c82509aaaab5e8a0ab19390c183e624e3a22c174
Filipe Encarnacao <filipe11402@outlook.pt>
2021-09-10 09:45:43 +0100

    added movies table

---
+++ Movies.Infrastructure/InjectDependencies.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Infrastructure
+{
+    public static class InjectDependencies
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    configuration.GetConnectionString("MovieConnection"), b => b.MigrationsAssembly("Movies.Infrastructure"));
+            });
+        }
+    }
+}
